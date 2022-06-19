2ab3febad0f5e429c52ae2265bf8bf029091ccc7
Ligil V James <ligil@Ligils-MacBook-Air.local>
2022-06-19 23:48:20 +0530

    added package and bg

--- App.tsx
+++ App.tsx
@@ -25,7 +25,7 @@ const App = () => {
   return (
 
 
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#C499BA' }}>
       <Home />
     </SafeAreaView>
   )
